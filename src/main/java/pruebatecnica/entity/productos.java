7ae8a2b27efe42801e034ac9dce89dacb06d7021
Wilmer <wilmixmer@gmail.com>
2025-12-09 19:16:40 -0500

    Se agrego la base de datos y se agrego las dependencias de lombook y de mysql

--- src/main/java/pruebatecnica/entity/productos.java
+++ src/main/java/pruebatecnica/entity/productos.java
@@ -1,79 +1,26 @@
 package pruebatecnica.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "productos")
 public class productos {
 
-    int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idProducto;
     String codigo;
     String nombre;
     String descripcion;
     String marca;
-    categoriaProductos categoria;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private categoriaProductos categoria;
+
     float precio;
 
-    public productos(int id, String codigo, String nombre, String descripcion, String marca, categoriaProductos categoria,
-                     float precio) {
-        this.id = id;
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.marca = marca;
-        this.categoria = categoria;
-        this.precio = precio;
-    }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDescripcion() {
-        return descripcion;
-    }
-
-    public void setDescripcion(String descripcion) {
-        this.descripcion = descripcion;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public categoriaProductos getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(categoriaProductos categoria) {
-        this.categoria = categoria;
-    }
-
-    public float getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(float precio) {
-        this.precio = precio;
-    }
 }
